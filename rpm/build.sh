6499caa7fc409a95e73c95074277d3e3a1e9d97b
Gabriele Iannetti <g.iannetti@gsi.de>
2022-04-08 12:14:40 +0200

    Update rpm/build.sh for better readability.

--- rpm/build.sh
+++ rpm/build.sh
@@ -13,10 +13,10 @@ mkdir -p $builddir/SOURCES/$promdir/etc/sysconfig
 mkdir -p $builddir/SOURCES/$promdir/etc/sudoers.d
 mkdir -p $builddir/SOURCES/$promdir/var/log/prometheus-lustre-exporter
 cp rpm/prometheus-lustre-exporter.spec $builddir/SPECS/
-cp systemd/prometheus-lustre-exporter.service $builddir/SOURCES/$promdir/usr/lib/systemd/system/prometheus-lustre-exporter.service
-cp systemd/prometheus-lustre-exporter.options $builddir/SOURCES/$promdir/etc/sysconfig
-cp sudoers/prometheus-lustre-exporter $builddir/SOURCES/$promdir/etc/sudoers.d
-cp lustre_exporter $builddir/SOURCES/$promdir/usr/bin
+cp systemd/prometheus-lustre-exporter.service $builddir/SOURCES/$promdir/usr/lib/systemd/system/
+cp systemd/prometheus-lustre-exporter.options $builddir/SOURCES/$promdir/etc/sysconfig/
+cp sudoers/prometheus-lustre-exporter $builddir/SOURCES/$promdir/etc/sudoers.d/
+cp lustre_exporter $builddir/SOURCES/$promdir/usr/bin/
 cd $builddir/SOURCES
 tar -czvf $promdir.tar.gz $promdir
 cd $builddir
